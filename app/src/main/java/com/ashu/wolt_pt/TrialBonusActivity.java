30278034522090f94096d80b1359e55d10f6b2ed
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-03-23 08:49:56 +0530

    recheck

---
+++ app/src/main/java/com/ashu/wolt_pt/TrialBonusActivity.java
@@ -0,0 +1,14 @@
+package com.ashu.wolt_pt;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TrialBonusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trial_bonus);
+    }
+}
